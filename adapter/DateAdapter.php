6d67984ce2f9ef84c69c3e754416d19b72dfc5f3
Romário Arruda <35845386+romarioarruda@users.noreply.github.com>
2022-10-22 10:29:06 -0300

    update Adapter to not use strategy
    injecting dependency directly at DateAdapter

--- adapter/DateAdapter.php
+++ adapter/DateAdapter.php
@@ -1,13 +1,15 @@
 <?php
-require_once "RoitDateProtocol.php";
+require_once "Moment.php";
+require_once "DateFns.php";
 
 class DateAdapter
 {
     public $api;
 
-    public function __construct(RoitDateProtocol $api)
+    public function __construct(string $date)
     {
-        $this->api = $api;
+        // $this->api = new DateFns($date);
+        $this->api = new Moment($date);
     }
   
     public function formatToDayMonthYear(): string
--- adapter/index.php
+++ adapter/index.php
@@ -1,16 +1,6 @@
 <?php
 require_once "DateAdapter.php";
-require_once "Moment.php";
-require_once "DateFns.php";
 
-echo "Date FNS:\n";
-$datefns = new DateAdapter(new DateFns('2022-09-05'));
+$date = new DateAdapter('2022-09-05');
 
-echo $datefns->formatToDayMonthYear();
-
-echo "\n\n";
-
-echo "Moment:\n";
-$moment = new DateAdapter(new Moment('2022-09-05'));
-
-echo $moment->formatToDayMonthYear();
+echo $date->formatToDayMonthYear();
